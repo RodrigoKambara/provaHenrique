329e565417e8510e0cde3ec594b5b3ae8ab3a11f
rodrigo-kambara <kambararodrigo@gmail.com>
2020-09-24 21:54:21 -0300

    Commit da splash screen

---
+++ app/src/main/java/com/example/prova/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.prova
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        splashScreen()
+    }
+    private fun splashScreen(){
+        val intent = Intent(this, HomeActivity::class.java)
+
+        Handler().postDelayed({
+            intent.change()
+        },3000)
+    }
+    private fun Intent.change() {
+        startActivity(this)
+        finish()
+    }
+}
+
+
